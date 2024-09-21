c189c37e37217809749e23ebc0de48491a93ae39
Andrey Romashev <andreygaranin.0@yandex.ru>
2024-09-21 12:40:48 +0700

    add tech slider for mobile

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,32 +1,51 @@
 import './index.sass'
 import Swiper from 'swiper/bundle';
 import 'swiper/css/bundle';
-
-let readMoreBtn = document.querySelector(".read-more");
-let readMoreIcon = readMoreBtn.querySelector(".read-more__icon");
-let readMoreText = readMoreBtn.querySelector(".read-more__text");
-let pagination = document.querySelector(".swiper-pagination");
-let brandsContainer = document.querySelector(".brands-container");
-let brandsContainerList = document.querySelector(".brands-container__list");
-
-// Выбираем ВСЕ элементы в массив, чтобы пройтись по нему методом forEach
-let brandsContainerElements = document.querySelectorAll(".brands-container__element");
+// Общая пагинация
+let pagination = document.querySelectorAll(".swiper-pagination");
+// Селекторы секции брендов
+let brands = document.querySelector(".brands")
+let readMoreBrands = brands.querySelector(".read-more");
+let brandsList = document.querySelector(".brands__list");
+let brandsElements = document.querySelectorAll(".brands__element");
+let readMoreIcon = readMoreBrands.querySelector(".read-more__icon");
+let readMoreText = readMoreBrands.querySelector(".read-more__text");
+// Селекторы секции видов техниики
+let techs = document.querySelector(".tech")
+let readMoreTechs = techs.querySelector(".read-more");
+let techList = document.querySelector(".tech__list");
+let techElements = document.querySelectorAll(".tech__element");
 
 // Измеряем размер экрана
 const screenWidth = window.screen.width
 
 const checkScreen = (screen) => {
   if (screen < 520) { // Условия для мобильной версии
-    pagination.style = "visibility: visible";
-    readMoreBtn.style = "display: none";
-    brandsContainer.classList.add("swiper-container");
-    brandsContainerList.classList.add("swiper-wrapper");
-    brandsContainerElements.forEach((e) => e.classList.add("swiper-slide"));
-    const swiper = new Swiper('.swiper-container', {
+    pagination.forEach(e => e.style = "visibility: visible")
+    // Слайдер для брендов
+    readMoreBrands.style = "display: none";
+    brands.classList.add("swiper-container");
+    brandsList.classList.add("swiper-wrapper");
+    brandsElements.forEach(e => e.classList.add("swiper-slide"));
+    const brandsSwiper = new Swiper('.brands ', {
       slidesPerView: 1,
       spaceBetween: 16,
       width: 240,
       height: 72,
+      pagination: {
+        el: '.swiper-pagination',
+      },
+    });
+    // Слайдер для видов техники
+    readMoreTechs.style = "display: none";
+    techs.classList.add("swiper-container");
+    techList.classList.add("swiper-wrapper");
+    techElements.forEach(e => e.classList.add("swiper-slide"));
+    const techSwiper = new Swiper('.tech ', {
+      slidesPerView: 1,
+      spaceBetween: 16,
+      width: 240,
+      height: 160,
       pagination: {
         el: '.swiper-pagination',
       },
@@ -38,35 +57,14 @@ checkScreen(screenWidth)
 
 const readMore = () => {
   if (readMoreText.textContent === 'Показать все') {
-    brandsContainerList.style = "animation: open 1s"
-    if (screenWidth < 1120 && screenWidth > 754) {
-      console.log(screenWidth)
-      setTimeout(() => {
-        brandsContainerList.style = "height: 336px"
-      }, 1000)
-    } else if (screenWidth < 754) {
-      setTimeout(() => {
-        brandsContainerList.style = "height: 512px"
-      }, 1000)
-    } else if (screenWidth > 1120) {
-      setTimeout(() => {
-        brandsContainerList.style = "height: 248px"
-      }, 1000)
-
-      readMoreIcon.style = 'transform: rotate(0)'
-      readMoreText.textContent = 'Показать все';
-    }
+    brandsList.style = "height: 100%"
     readMoreIcon.style = 'transform: rotate(180deg)'
     readMoreText.textContent = 'Скрыть';
   } else {
-    brandsContainerList.style = "animation: close 1s"
-    setTimeout(() => {
-      brandsContainerList.style = "height: 160px"
-    }, 1000)
-
+    brandsList.style = "height: 160px"
     readMoreIcon.style = 'transform: rotate(0)'
     readMoreText.textContent = 'Показать все';
   }
 }
 
-readMoreBtn.addEventListener('click', readMore)
+readMoreBrands.addEventListener('click', readMore)
